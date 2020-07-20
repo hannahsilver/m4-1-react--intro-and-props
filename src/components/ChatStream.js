0272a4b22a5b645fd6d68bb49bb703d8778db408
Hannah Silver <hannah_silver@hotmail.com>
2020-07-20 13:42:04 -0400

    exercise 1

--- src/components/ChatStream.js
+++ src/components/ChatStream.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './ChatStream.css';
+import "./ChatStream.css";
+import { users } from "../data";
 
 // The current user's messages should have:
 // - a text color of #FFF
@@ -11,7 +12,14 @@ import './ChatStream.css';
 // - a background color of #e9e9eb
 
 const ChatStream = (props) => {
-  return <section className='chat-stream'>{/* Your code here! */}</section>;
+  console.log(props);
+  return (
+    <section className="chat-stream">
+      {props.messages.map((message) => (
+        <message>{message.body}</message>
+      ))}
+    </section>
+  );
 };
 
 export default ChatStream;
